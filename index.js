f6067fa35853b3061bdb3d9383b8c5df95cd6ac2
ふじえもん <105954632+shotafujie@users.noreply.github.com>
2024-04-19 00:54:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,61 @@
-var kuromoji = require("kuromoji");
-const sleep = require('./sleep');
-var jsdom = require('jsdom');
-$ = require('jquery')(new jsdom.JSDOM().window);
-
-
-//単語取り出しに使う変数
-var word = ""; //pos=="名詞"
-
+const kuromoji = require("kuromoji");
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-//クライアントからAが送られたときの処理
-io.on('connection', function(socket) {
-  //Aを受け取ると，その値をCに渡す
-  socket.on("A", async function(value) {
-    const result = await C(value);
-    socket.emit("B", result);
+
+let lastSentWords = [];
+
+let tokenizer;
+kuromoji.builder({ dicPath: "node_modules/kuromoji/dict" }).build((err, builtTokenizer) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  tokenizer = builtTokenizer;
+});
+
+io.on('connection', (socket) => {
+  socket.on("send", async (value) => {
+    if (tokenizer) {
+      const result = await extractNouns(value);
+      socket.emit("draw", result);
+    }
   });
 });
 
-
-async function C(value) {
+async function extractNouns(value) {
   return new Promise((resolve, reject) => {
-    kuromoji.builder({ dicPath: "node_modules/kuromoji/dict" }).build(function(err, tokenizer) {
-      var path = tokenizer.tokenize(value);
-      //console.log(path);
-      const result = new Array();
-      word = "";
-      //入力された値すべてをチェック
-      for (let i = 0; i < path.length; i++) {
-        //名詞以外ならば続ける
-        if (path[i].pos === '名詞') {
-          word += path[i].surface_form;
-        } else {
-          if (word !== '') {
-            result.push(word);
-            word = '';
-          }
+    const tokens = tokenizer.tokenize(value);
+    const newWords = [];
+    let currentNoun = "";
+
+    for (const token of tokens) {
+      if (token.pos === '名詞') {
+        currentNoun += token.surface_form;
+      } else {
+        if (currentNoun) {
+          newWords.push(currentNoun);
+          currentNoun = '';
         }
       }
-      if (word !== '') {
-        result.push(word);
-      }
-      resolve(result);
-      console.log(result);
-      
-    });
+    }
+
+    if (currentNoun) {
+      newWords.push(currentNoun);
+    }
+
+    const uniqueWords = newWords.filter(word => !lastSentWords.includes(word));
+    lastSentWords = newWords;
+
+    resolve(uniqueWords);
   });
 }
-//ここまで名詞に分割している
-
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  // es.sendFile(__dirname + '/public/index.html');
-});
+const port = process.env.PORT || 3000;
 
-http.listen(3000, () => {
-  console.log("listening on *:3000");
+http.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
-
