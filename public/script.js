82c83164c2449481ef91ba211b5ecea43f71bbe8
ふじえもん <105954632+shotafujie@users.noreply.github.com>
2024-04-19 00:54:18 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,76 +1,43 @@
 const socket = io();
-//Web Speech APIのSpeechRecognitionインタフェースのオブジェクトを作成
-SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
-const sr = new SpeechRecognition();
-//ボタンの状況を格納
-const btn_s = document.getElementById("startButton");
-//node_jsボタン
-const btn_n = document.getElementById("NodeButton");
-//htmlからID引っ張って名前つける
-const log = document.getElementById("textLog");
-let empty = "<br>";
-//見やすさのため改行できるように。
-//ターゲットに特定のイベントが配信されるたびに呼び出される関数を設定
-//1つのイベントに対して複数のハンドラーを追加することができる
-//音声認識サービスが停止した際には，音声認識を停止及びボタンを非表示
-sr.addEventListener("end", function(){
-  sr.stop();
-  btn_s.disabled = false;
-})
-sr.addEventListener("result", function(e) {
-  console.log(e.results);
-  log.innerHTML = "";
-  for (var i = 0; i < e.results.length; i++) {
-    log.innerHTML += e.results[i][0].transcript + "。";
-    //認識が終わったらstartは表示状態    
-    btn_s.disabled = true;
+const SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+const recognition = new SpeechRecognition();
+const startButton = document.getElementById("startButton");
+const micIcon = document.getElementById("micIcon");
+const logElement = document.getElementById("textLog");
+
+recognition.addEventListener("end", () => {
+  micIcon.src="./microphone.png";
+  recognition.stop();
+});
+
+recognition.addEventListener("result", (e) => {
+  logElement.innerHTML = "";
+  for (const result of e.results) {
+    logElement.innerHTML += result[0].transcript + "。";
   }
 });
 
+function startRecognition() {
+  logElement.innerHTML = "";
+  recognition.interimResults = true;
+  recognition.continuous = true;
+  micIcon.src="./microphone_rec.png";
+  recognition.start();
+  sendWordsToServer();
+  setInterval(sendWordsToServer, 5000);
+}
 
-btn_s.addEventListener("click", function() {
-  document.getElementById("description").innerHTML = "音声認識結果  ~サーバーに送信で反映します~"
-  //音声認識をリセットする
-  log.innerHTML = "";
-  //認識の途中経過がONの状態
-  sr.interimResults = true;
-  //連続音声認識CSRがONの状態
-  sr.continuous = true;
-  //認識開始  
-  sr.start();
-  sendwords();
-  //sr.stop(); //ボタンを押したらstopする
-  
-  //1０秒ごとに関数を実行するタイマーの設定 第1引数は呼び出す関数名,第2引数は繰り返す時間をms表記で指定.
-  setInterval(sendwords,10000);
-
-});
-
-//10秒経つごとに呼ばれる.
-function sendwords(){
-  document.getElementById("description").innerHTML = "話したことばが出てくるよ！！"
-
-  var string = log.innerHTML.replace(/<br>/g, "")
-  socket.emit("A", string)
-  log.innerHTML = "";  
-  
+function sendWordsToServer() {
+  const recognizedText = logElement.innerHTML.replace(/<br>/g, "");
+  socket.emit("send", recognizedText);
+  $('#container').empty();
+  logElement.innerHTML = "";
 }
 
 
-//B
-socket.on("B", function(result) {
-  draw(result);
-})
-
-
-
-//描写用関数
-function draw(text) {
-  console.log("渡されたよ");//draw()が動いているかを確認
+function drawReceivedData(text) {
   const randRange = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-  //ランダム関数
   let html = '';
-  //containerはjQueryオブジェクト
   let container = $('.container');
   for (let i = 0; i < text.length; i++) {
     html += '<p class="text" style="animation-duration:' + randRange(8, 15) + 's;animation-delay:' + randRange(1, 10) + 's;">'
@@ -78,7 +45,7 @@ function draw(text) {
       + '</p>';
   }
   
-  console.log(html); //分割された名詞ごとに，pタグが含まれ，cssによる動き方の情報が入っている
+  //console.log(html); //分割された名詞ごとに，pタグが含まれ，cssによる動き方の情報が入っている
   $('.container').append(html);//流れるテキスト挿入
   // 流れるテキスト追加　ここまで
   let item = $('.container').find('p');
@@ -96,3 +63,7 @@ function draw(text) {
 
   
 }
+
+// Event listeners
+startButton.addEventListener("click", startRecognition);
+socket.on("draw", drawReceivedData);
